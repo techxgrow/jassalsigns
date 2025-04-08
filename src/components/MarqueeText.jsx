5e17647a42920d39315c16cbe7597c4039171ccf
anshuman619 <mishraanshuman619@gmail.com>
2025-04-08 20:18:52 +0530

    correcting marquee

--- src/components/MarqueeText.jsx
+++ src/components/MarqueeText.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/MarqueeText.css';
+
 
 const texts = [
   "ADDA SIGNS",
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-
+import "@/styles/MarqueeText.css";
 export default function App({ Component, pageProps }) {
   return (<>
 
