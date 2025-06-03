a2e4a4a0448c7c46256683cb667be326e11a8df0
anshuman619 <mishraanshuman619@gmail.com>
2025-06-03 17:15:06 +0530

    making the css file module file

--- src/components/CitySlider.jsx
+++ src/components/CitySlider.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import SmallTextAnimation from "./ui/SmallTextAnimation";
+
 import Link from "next/link";
 
 export default function CitySlider({ cityName }) {
--- src/components/ui/SmallTextAnimation.jsx
+++ src/components/ui/SmallTextAnimation.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import "../../styles/SmallTextAnimation.css";
+import styles from "../../styles/SmallTextAnimation.module.css";
 
 const SmallTextAnimation = (props) => {
   const { text, textColor, fontSize } = props;
 
   return (
-   <div
-  className="page"
-  style={{
-    "--font-size": fontSize,
-    "--text-gradient": textColor, // for gradient input
-  }}
->
-  <h1 className="text page__text " data-text={text}>
-    {text}
-  </h1>
-</div>
+    <div
+      className={styles.page}
+      style={{
+        "--font-size": fontSize,
+        "--text-gradient": textColor, // for gradient input
+      }}
+    >
+      <h1 className={`${styles.text} `} data-text={text}>
+        {text}
+      </h1>
+    </div>
   );
 };
 
