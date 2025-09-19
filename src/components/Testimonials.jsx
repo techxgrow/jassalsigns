b95e0c4ea8f4533a08da9b14809bea37309cb3e3
Kunal Sharma <kunalpandit5200@gmail.com>
2025-09-20 00:59:01 +0530

    resolve build

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,41 +9,54 @@ const testimonials = [
     clientImg: "/testimonials/user-1.jpg",
     clientName: "Vita Denture Cinic",
     category: "CEO, Tech Innovations",
-    title: "I am so thankful to Jassal Signs and Harry. We had an event for which Jassal Signs made our banner. ... Thank you",
+    title:
+      "I am so thankful to Jassal Signs and Harry. We had an event for which Jassal Signs made our banner. ... Thank you",
     link: "#",
+    id: 1,
   },
   {
-    image: "https://outmedia.freekytheme.com/wp-content/uploads/2020/12/blog-details-d.jpg",
+    image:
+      "https://outmedia.freekytheme.com/wp-content/uploads/2020/12/blog-details-d.jpg",
     clientImg: "/testimonials/user-2.jpg",
     clientName: "Meenu Dahal",
     category: "Marketing Head, Creative Agency",
-    title: "I am super happy with the products and the customer services (especially Gagan, Vik and the receptionist were very professional and they know what they are doing.",
+    title:
+      "I am super happy with the products and the customer services (especially Gagan, Vik and the receptionist were very professional and they know what they are doing.",
     link: "#",
   },
   {
-    image: "https://outmedia.freekytheme.com/wp-content/uploads/2020/09/blog-details-a.jpg",
+    image:
+      "https://outmedia.freekytheme.com/wp-content/uploads/2020/09/blog-details-a.jpg",
     clientImg: "/testimonials/user-3.jpg",
     clientName: "David Johnson",
     category: "Founder, Startup Hub",
-    title: "Throughout the whole buying experience from walking in with no vision to walking out with the most eye catching sign, Jassal Signs was like no other when it comes to customer service.",
+    title:
+      "Throughout the whole buying experience from walking in with no vision to walking out with the most eye catching sign, Jassal Signs was like no other when it comes to customer service.",
     link: "#",
   },
   {
-    image: "https://outmedia.freekytheme.com/wp-content/uploads/2020/12/blog-details-f.jpg",
+    image:
+      "https://outmedia.freekytheme.com/wp-content/uploads/2020/12/blog-details-f.jpg",
     clientImg: "/testimonials/user-4.jpg",
     clientName: "Emma Brown",
     category: "Founder",
-    title: "Their signage service was absolutely fantastic. The quality, communication, and efficiency made this experience far better than I ever imagined possible.",
+    title:
+      "Their signage service was absolutely fantastic. The quality, communication, and efficiency made this experience far better than I ever imagined possible.",
     link: "#",
   },
 ];
 
 export default function Testimonials() {
   return (
-    <section className="pt-20 pb-16 bg-black mb-[60px] md:mb-0 px-4 " id="testimonials">
+    <section
+      className="pt-20 pb-16 bg-black mb-[60px] md:mb-0 px-4 "
+      id="testimonials"
+    >
       <div className="md:max-w-[85vw] max-w-[95vw] mx-auto text-center">
         <div className="mb-[30px]" data-aos="fade-up">
-          <p className="md:text-2xl text-xl text-white uppercase mb-3">What Our Clients Say</p>
+          <p className="md:text-2xl text-xl text-white uppercase mb-3">
+            What Our Clients Say
+          </p>
           <h1 className="md:text-6xl text-4xl text-white font-extrabold">
             TESTIMONIALS<span className="text-orange-600"></span>
           </h1>
@@ -62,9 +75,7 @@ export default function Testimonials() {
         >
           {testimonials.map((testimonial, idx) => (
             <SwiperSlide key={idx}>
-              <div
-                className="block md:h-70 h-50  overflow-hidden shadow-md group"
-              >
+              <div className="block md:h-70 h-50  overflow-hidden shadow-md group">
                 <div
                   className="h-full bg-cover bg-center bg-gray-700 transition-transform duration-300 parent-testimonial"
                   // style={{ backgroundImage: `url(${testimonial.image})` }}
@@ -76,10 +87,14 @@ export default function Testimonials() {
                         alt={testimonial.clientName}
                         className="w-12 h-12 rounded-full border-2 border-white mr-3"
                       />
-                      <h1 className="text-2xl font-medium">{testimonial.clientName}</h1>
+                      <h1 className="text-2xl font-medium">
+                        {testimonial.clientName}
+                      </h1>
                     </div>
 
-                    <p className="text-sm sm:text-base text-left mb-2 z-30">{testimonial.title}</p>
+                    <p className="text-sm sm:text-base text-left mb-2 z-30">
+                      {testimonial.title}
+                    </p>
                   </div>
                 </div>
               </div>
