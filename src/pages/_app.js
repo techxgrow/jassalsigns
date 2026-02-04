65d315bfa3e66ba86a61201d06cb42ba48eeae07
Kunal Sharma <kunalpandit5200@gmail.com>
2026-02-04 14:21:04 +0530

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/MarqueeText.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <main>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </main>
   );
