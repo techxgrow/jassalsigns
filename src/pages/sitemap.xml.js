f725b80003b361bad42513d598f5b9c264f50d03
TechXGrow Limited <techxgrow@outlook.com>
2026-05-26 16:06:00 -0600

    feat: add dynamic sitemap generation for static, product, blog, and city pages

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,118 +1,60 @@
+import { data } from '../../assets/data.js';
+
 const URL = 'https://www.jassalsignsedm.com';
 
 function generateSiteMap() {
   const currentDate = new Date().toISOString().split('T')[0];
 
+  // 1. Static Pages
+  const staticPages = [
+    { path: '', changefreq: 'daily', priority: '1.0' },
+    { path: 'about', changefreq: 'monthly', priority: '0.8' },
+    { path: 'contact', changefreq: 'monthly', priority: '0.8' },
+    { path: 'services', changefreq: 'weekly', priority: '0.8' },
+    { path: 'franchise', changefreq: 'monthly', priority: '0.8' },
+    { path: 'gallery', changefreq: 'weekly', priority: '0.8' },
+    { path: 'blogs', changefreq: 'weekly', priority: '0.8' }
+  ];
+
+  const staticUrls = staticPages.map(page => `
+  <url>
+    <loc>${URL}/${page.path}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>${page.changefreq}</changefreq>
+    <priority>${page.priority}</priority>
+  </url>`).join('');
+
+  // 2. Dynamic Product Pages
+  const productKeys = Object.keys(data.productPage);
+  const productUrls = productKeys.map(key => `
+  <url>
+    <loc>${URL}/products/${key}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>`).join('');
+
+  // 3. Dynamic Blog Pages
+  const blogUrls = data.blogPage.map(blog => `
+  <url>
+    <loc>${URL}/blogs/${blog.id}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`).join('');
+
+  // 4. City Pages
+  const cities = ['EDMONTON', 'CALGARY', 'SURREY', 'CLOVERDALE', 'ABBOTSFORD'];
+  const cityUrls = cities.map(city => `
+  <url>
+    <loc>${URL}/citypage/${city}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${city === 'EDMONTON' ? '0.9' : '0.8'}</priority>
+  </url>`).join('');
+
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${URL}/</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>${URL}/about</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/contact</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/services</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/franchise</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/gallery</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/products/vehiclewraps</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/products/channelletters</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/products/printmedia</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/products/pylonsigns</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/products/indoorsigns</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/products/outdoorsigns</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/products/otherproducts</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/citypage/EDMONTON</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>${URL}/citypage/CALGARY</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/citypage/SURREY</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/citypage/CLOVERDALE</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${URL}/citypage/ABBOTSFORD</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${staticUrls}${productUrls}${blogUrls}${cityUrls}
 </urlset>
 `;
 }
